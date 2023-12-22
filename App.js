d92c203e186f11eae6473f7b9ca6e844c662ffc9
Aariaanit <arianittershnjaku@gmail.com>
2023-12-22 14:50:24 +0100

    22.12.2023

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Linking, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Bubu e don Dudu 100%!</Text>
+      <Button color="red" title='Google' onPress={()=>{Linking.openURL('https://google.com')}}></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color:'red',
+    fontSize:'25px',
+    justifyContent:'center',
+  }
+
 });
